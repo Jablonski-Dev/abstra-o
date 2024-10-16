b8d486f817e43fe0e5fcb66ea1e9943c2cea089c
Felipe jablonski <felipe.j.silva8@aluno.senai.br>
2024-10-16 10:46:13 -0300

    poo

--- 02 - abstração/02 - abstração/obj/Release/net8.0/02 - abstração.AssemblyInfo.cs
+++ 02 - abstração/02 - abstração/obj/Release/net8.0/02 - abstração.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("01 - heranca")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("02 - abstração")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+992be756df783ea9ecdc8dfe204b717bd027d195")]
-[assembly: System.Reflection.AssemblyProductAttribute("01 - heranca")]
-[assembly: System.Reflection.AssemblyTitleAttribute("01 - heranca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("02 - abstração")]
+[assembly: System.Reflection.AssemblyTitleAttribute("02 - abstração")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
--- Ex05-Funcionarios/Ex05-Funcionarios/obj/Debug/net8.0/Ex05-Funcionarios.AssemblyInfo.cs
+++ Ex05-Funcionarios/Ex05-Funcionarios/obj/Debug/net8.0/Ex05-Funcionarios.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("01 - heranca")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Ex05-Funcionarios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+992be756df783ea9ecdc8dfe204b717bd027d195")]
-[assembly: System.Reflection.AssemblyProductAttribute("01 - heranca")]
-[assembly: System.Reflection.AssemblyTitleAttribute("01 - heranca")]
+[assembly: System.Reflection.AssemblyProductAttribute("Ex05-Funcionarios")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Ex05-Funcionarios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
